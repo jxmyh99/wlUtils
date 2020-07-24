cef32b6b49fb115d80406868483ee51da6eb2003
黄新民 <huangxinmin@hzwanglan.com>
2020-07-24 11:02:32 +0800

    增加clear-dir删除无用的文件

--- script/build-copy.js
+++ script/build-copy.js
@@ -3,33 +3,32 @@
  * @Github: http://www.33u3.com
  * @Date: 2019-08-12 10:26:46
  * @LastEditors: 明月寒
- * @LastEditTime: 2019-08-12 11:32:16
+ * @LastEditTime: 2020-07-24 10:54:14
  * @Description: 复制文件
  */
-const path = require("path")
-const fs = require("fs")
-const ora = require("ora")
-const rm = require("rimraf")
-const copy = require("copy")
-const chalk = require("chalk")
+const path = require('path');
+const fs = require('fs');
+const ora = require('ora');
+const rm = require('rimraf');
+const copy = require('copy');
+const chalk = require('chalk');
 
+const rootPath = path.resolve(__dirname, '../');
 
-const rootPath = path.resolve(__dirname,"../")
-
-let copying = ora("copying....")
+let copying = ora('Loading copying....');
 copying.start();
 
-rm("*.js",(err,data)=>{
-    if(err) throw err
-    let folderList = fs.readdirSync(path.resolve(rootPath,'src'))
-    folderList.forEach((item,index)=>{
-        copy(`src/${item}/*.js`,rootPath,(error,files)=>{
-            console.log(error)
-            if(error) throw error
-            if(index === folderList.length -1){
-                console.log(' Copy complete.\n')
-                copying.stop();
-            }
-        })
-    })
-})
+rm('*.js', (err, data) => {
+  if (err) throw err;
+  let folderList = fs.readdirSync(path.resolve(rootPath, 'src'));
+  folderList.forEach((item, index) => {
+    copy(`src/${item}/*.js`, rootPath + '/dis/', (error, files) => {
+      console.log(error);
+      if (error) throw error;
+      if (index === folderList.length - 1) {
+        console.log(' Copy complete.\n');
+        copying.stop();
+      }
+    });
+  });
+});
