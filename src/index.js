5a2773f4db927d8b05fc892e80ffb894a30c8b86
黄新民 <huangxinmin@hzwanglan.com>
2020-12-31 10:44:48 +0800

    修改版本号

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@
  * @Github: http://www.33u3.com
  * @Date: 2019-08-12 10:50:43
  * @LastEditors: 明月寒
- * @LastEditTime: 2020-07-24 10:23:21
+ * @LastEditTime: 2020-12-31 10:44:34
  * @Description: webpack打包入口文件
  * @Example: 自动引入子目录下所有js文件
  */
 let moduleExports = {
-  ver: '1.1.7',
+  ver: "1.1.8",
 };
 
-const r = require.context('./', true, /^\.\/.+\/.+\.js$/);
+const r = require.context("./", true, /^\.\/.+\/.+\.js$/);
 r.keys().forEach((key) => {
-  let attr = key.substring(key.lastIndexOf('/') + 1, key.lastIndexOf('.'));
+  let attr = key.substring(key.lastIndexOf("/") + 1, key.lastIndexOf("."));
   moduleExports[attr] = r(key);
 });
 
